aa2dbaa1493c8325a930b8614993e4f0641b1e08
Stefan Malzner <stefan@adlk.io>
2017-10-16 17:50:24 +0200

    initial commit

---
+++ webview.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    const elements = document.querySelectorAll('.unread');
+    let count = 0;
+
+    for (let i = 0; i < elements.length; i += 1) {
+      if (elements[i].querySelectorAll('.icon-muted').length === 0) {
+        count += 1;
+      }
+    }
+
+    // set Franz badge
+    Franz.setBadge(count);
+  };
+
+  // inject franz.css stylesheet
+  Franz.injectCSS(path.join(__dirname, 'service.css'));
+
+  // check for new messages every second and update Franz badge
+  Franz.loop(getMessages);
+};
