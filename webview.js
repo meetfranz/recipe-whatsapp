5700e8fa940a359931abbbf0fe414685ffee25a5
Stefan Malzner <stefan@adlk.io>
2019-06-04 12:11:15 +0200

    Ignore muted conversations

--- webview.js
+++ webview.js
@@ -30,11 +30,11 @@ window.addEventListener('beforeunload', async () => {
 
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-    const elements = document.querySelectorAll('.CxUIE, .unread, .P6z4j');
+    const elements = document.querySelectorAll('.CxUIE, .unread, ._0LqQ');
     let count = 0;
 
     for (let i = 0; i < elements.length; i += 1) {
-      if (elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
+      if (elements[i].querySelector('.P6z4j').length === 1 && elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
         count += 1;
       }
     }
