a885bf25aee1ca0ef90c3786cd909799b8ce7301
Stefan Malzner <stefan@adlk.io>
2021-12-09 11:23:31 +0100

    Fix css selector

--- webview.js
+++ webview.js
@@ -1,8 +1,4 @@
-const { remote } = require('electron');
 const path = require('path');
-
-const webContents = remote.getCurrentWebContents();
-const { session } = webContents;
 
 setTimeout(() => {
   const elem = document.querySelector('.landing-title.version-title');
@@ -11,36 +7,24 @@ setTimeout(() => {
   }
 }, 1000);
 
-const isMutedIcon = element => element.parentElement.parentElement.querySelectorAll('*[data-icon="muted"]').length !== 0;
+const isMutedIcon = element =>
+  element.parentElement.parentElement.querySelectorAll('*[data-icon="muted"]')
+    .length !== 0;
 
 const isPinnedIcon = element => element.classList.contains('_1EFSv');
 
-window.addEventListener('beforeunload', async () => {
-  try {
-    session.flushStorageData();
-    session.clearStorageData({
-      storages: ['appcache', 'serviceworkers', 'cachestorage', 'websql', 'indexdb'],
-    });
-
-    const registrations = await window.navigator.serviceWorker.getRegistrations();
-
-    registrations.forEach((r) => {
-      r.unregister();
-      console.log('ServiceWorker unregistered');
-    });
-  } catch (err) {
-    console.err(err);
-  }
-});
-
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-    const elements = document.querySelectorAll('.CxUIE, .unread, ._0LqQ, .m61XR .ZKn2B, .VOr2j, ._1V5O7 ._2vfYK, html[dir] ._23LrM');
+    const elements = document.querySelectorAll(
+      '.CxUIE, .unread, ._0LqQ, .m61XR .ZKn2B, .VOr2j, ._1V5O7 ._2vfYK, html[dir] ._23LrM, ._1pJ9J',
+    );
+
+    console.log(elements);
     let count = 0;
 
     for (let i = 0; i < elements.length; i += 1) {
       try {
-      // originalLog(isMutedIcon(elements[i]), isPinnedIcon(elements[i]));
+        // originalLog(isMutedIcon(elements[i]), isPinnedIcon(elements[i]));
         if (!isMutedIcon(elements[i]) && !isPinnedIcon(elements[i])) {
           count += 1;
         }
